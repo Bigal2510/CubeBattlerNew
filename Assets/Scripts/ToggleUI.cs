4a689e9bc862df19aa88449e7ed4c0cf17779477
Bigal2510 <jones7ax@mail.uc.edu>
2025-04-25 14:08:59 -0400

    hopefully this gets my game up

---
+++ Assets/Scripts/ToggleUI.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+public class ToggleUI : MonoBehaviour
+{
+    public GameObject networkUIPanel;
+    private Boolean toggle = true;
+
+
+    public void ToggleUIButtonHandler()
+    {
+        toggle = !toggle;
+        networkUIPanel.SetActive(toggle);
+    }
+
+}
